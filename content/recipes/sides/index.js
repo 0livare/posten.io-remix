76ad0a65b0b4d4f6efe1147e46f35ad235599c3e
Zach Posten <zach@posten.io>
2021-12-19 13:19:05 -0600

    chore: Convert posts to MDX

--- content/recipes/sides/index.js
+++
@@ -1,4 +0,0 @@
-import * as mashedPotatoes from './recipe-mashed-potatoes.md'
-import * as twiceBakedPotatoes from './recipe-twice-baked-potatoes.md'
-
-export const sides = [mashedPotatoes, twiceBakedPotatoes]
