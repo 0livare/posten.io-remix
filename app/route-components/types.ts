bb7a887c35f49a428676d73b64c47979f2dd7fe0
Zach Posten <zach@posten.io>
2021-12-19 17:03:04 -0600

    feat: Try importing MDX directly

--- app/route-components/types.ts
+++ app/route-components/types.ts
@@ -1,4 +1,12 @@
-export type Post = {
+export type PostFrontMatter = {
+  title: string
+  description: string
+  imageFilename?: string
+  date: string
+  slug: string
+}
+
+export type Post = PostFrontMatter & {
   title: string
   description: string
   imageFilename?: string
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+
+declare module '*.mdx' {
+  export const frontmatter: Record<string, unknown>
+  export const title: string
+}
